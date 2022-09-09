33b90b002349c108ae35935e34c8401b95ea8578
aleema95 <alejandro.manouellian@gmail.com>
2022-09-09 14:13:45 -0300

    created navbar and added i18next lib for multiple languages

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,8 +1,30 @@
+import { Link } from 'react-router-dom'
+import s from './Navbar.module.scss'
+import { useTranslation } from 'react-i18next'
 
 export default function Navbar() {
+  const [t, i18n] = useTranslation<string>("global")
+
+  function handleLanguageChange(e: React.MouseEvent<HTMLButtonElement>) {
+    const target = e.target as HTMLButtonElement;
+    const language = target.value
+    
+    i18n.changeLanguage(language)
+  }
+
   return (
-    <div>
-      <h1>Home</h1>
+    <div className={s.mainContainer}>
+      <ul className={s.listContainer}>
+        <Link className={s.mainLink} to='/'>Alejandro Manouellian</Link>
+        <div className={s.navLinksContainer}>
+          <Link className={s.link} to='/'>{t('navbar.home')}</Link>
+          <Link className={s.link} to='/About'>{t('navbar.about')}</Link>
+        </div>
+      </ul>
+      <div className={s.languageChangeBtnContainer}>
+        <button value="es" onClick={handleLanguageChange}>{t('navbar.lng-es')}</button>
+        <button value="en" onClick={handleLanguageChange}>{t('navbar.lng-en')}</button>
+      </div>
     </div>
   )
 }
