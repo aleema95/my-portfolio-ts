0b6630027cb6a78d121278b9d030f3148c3b2ae3
aleema95 <alejandro.manouellian@gmail.com>
2022-09-29 16:04:28 -0300

    commit?

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import Navbar from './components/Navbar/Navbar';
-import Portfolio from './components/Portfolio/Portfolio';
 
 function App() {
   return (
@@ -12,7 +11,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/About" element={<About />}/>
-        <Route path="/Portfolio" element={<Portfolio />}/>
       </Routes>
     </div>
   );
--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -1,31 +1,44 @@
 import React, { useState } from 'react'
+import { useTranslation } from 'react-i18next'
 import s from './Portfolio.module.scss'
 import JobHub from '../../assets/images/JobHub.png'
 import gameFlow from '../../assets/images/gameFlow.png'
-import { useSpring, animated } from 'react-spring'
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 
-export default function Portfolio() {
-  const fadeIn = useSpring({to: {opacity: 1}, from: { opacity: 0}})
+type Props = {
+  carouselSection: React.LegacyRef<HTMLDivElement>
+}
+
+export default function Portfolio(props: Props) {
+  const { carouselSection } = props;
+  const [t, i18n] = useTranslation<string>("global")
   
   return (
     // <animated.div style={fadeIn}>
     //   <h1 className={s.Title}>Portfolio</h1>  
     // </animated.div>
-    <section className={s.mainContainer}>
-      <div className={s.carouselContainer}>
-        <Carousel>
-          <div>
-              <img src={JobHub} />
-              <p className="legend">JobHub</p>
-          </div>
-          <div>
-              <img src={gameFlow} />
-              <p className="legend">GameFlow</p>
-          </div>
-        </Carousel>
+      <div ref={carouselSection} className={s.mainContainer}>
+        <h1 className={s.title}>{t('carousel.title')}</h1>
+        <div className={s.carouselContainer}>
+          <Carousel 
+            emulateTouch={true} 
+            infiniteLoop={true}
+            >
+            <div>
+                <img src={JobHub} />
+              <a href="https://jobhub.vercel.app/">
+                <p className="legend">JobHub</p>
+              </a>
+            </div>
+            <div>
+                <img src={gameFlow} />
+                <a href="https://gameflow.vercel.app/">
+                  <p className="legend">GameFlow</p>
+                </a>
+            </div>
+          </Carousel>
+        </div>
       </div>
-    </section>
   )
 }
