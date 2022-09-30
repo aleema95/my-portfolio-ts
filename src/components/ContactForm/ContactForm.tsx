11bfea1285d5d4696525e5543380a19a04e7abb4
aleema95 <alejandro.manouellian@gmail.com>
2022-09-30 15:55:28 -0300

    added language change to component

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -1,30 +1,33 @@
 import React from 'react'
 import s from './ContactForm.module.scss'
+import { useTranslation } from 'react-i18next'
 
 export default function ContactForm() {
+  const [t, i18n] = useTranslation<string>("global")
+
   return (
     <div className={s.formContainer}>
-      <h1 className={s.contactText}>CONTACT</h1>
+      <h1 className={s.contactText}>{t("contact.contactTitle_label")}</h1>
       <form action="" className={s.form}>
         <div className={s.nameLastNameContainer}>
           <div className={s.nameInputContainer}>
-            <label>Name</label>
+            <label>{t("contact.name_label")}</label>
             <input type="text" />
           </div>
           <div className={s.lastNameInputContainer}>
-            <label>Last Name</label>
+            <label>{t("contact.lastName_label")}</label>
             <input type="text" />
           </div>
         </div>
         <div className={s.emailInputContainer}>
-          <label>Email</label>
+          <label>{t("contact.email_label")}</label>
           <input type="text" />
         </div>
         <div className={s.messageInputContainer}>
-          <label>Message</label>
+          <label>{t("contact.message_label")}</label>
           <textarea></textarea>
         </div>
-        <input className={s.submitBtn} type="submit" />
+        <input className={s.submitBtn} type="submit" value={t("contact.submit_btn")}/>
       </form>
     </div>
   )
